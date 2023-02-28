bac54fb05416824509653d595ef6e6f77e59cc17
DanilVelichko <velichko.danil@gmail.com>
2023-02-28 21:35:23 +0100

    HW done

--- models/contactsFuncs.js
+++ models/contactsFuncs.js
@@ -5,19 +5,18 @@ const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
   try {
-
     const data = await fs.readFile(contactsPath, "utf-8"); // read file
     const contacts = JSON.parse(data); // parse data
     return contacts;
-  } catch (error) { 
+  } catch (error) {
     throw error;
   }
 };
 
 const getContactById = async (contactId) => {
   try {
-     const contacts = await listContacts();
-    const contact = contacts.find(c => c.id === contactId.toString()); // find contact by id
+    const contacts = await listContacts();
+    const contact = contacts.find((c) => c.id === contactId.toString()); // find contact by id
     return contact;
   } catch (error) {
     throw error;
@@ -29,11 +28,10 @@ const removeContact = async (contactId) => {
     const contacts = await listContacts();
     const newContacts = contacts.filter((item) => item.id !== contactId); // filter contacts
     if (contacts.length === newContacts.length) {
-       throw new Error("Contact not found");
-       
+      throw new Error("Contact not found");
     }
     await fs.writeFile(contactsPath, JSON.stringify(newContacts)); // write new array to file
-     
+
     return newContacts;
   } catch (error) {
     throw error;
@@ -44,7 +42,7 @@ const addContact = async (body) => {
   try {
     const contacts = await listContacts();
     const id = (contacts.length + 1).toString(); // generate id for new contact
-    const newContact = { ...body, id: id }; 
+    const newContact = { ...body, id: id };
     const newContacts = [...contacts, newContact];
     await fs.writeFile(contactsPath, JSON.stringify(newContacts)); // write new array to file
     console.log("New contact has been added!");
@@ -57,12 +55,12 @@ const addContact = async (body) => {
 const updateContact = async (contactId, body) => {
   try {
     const contacts = await listContacts(); // get contacts
-    const index = contacts.findIndex(c => c.id === contactId.toString()); // find index of contact
+    const index = contacts.findIndex((c) => c.id === contactId.toString()); // find index of contact
     if (index === -1) {
       throw new Error("Contact not found");
     }
     const updatedContact = { ...contacts[index], ...body, id: contactId }; // update contact
-    const newContacts = [...contacts]; 
+    const newContacts = [...contacts];
     newContacts[index] = updatedContact; // update contact in new array
     await fs.writeFile(contactsPath, JSON.stringify(newContacts)); // write new array to file
     return updatedContact;
