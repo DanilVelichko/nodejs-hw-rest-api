3c355bdf9d63d0862cc899bdb7b204d6759dd60c
DanilVelichko <velichko.danil@gmail.com>
2023-02-28 20:14:21 +0100

    HW done

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts"); // import router
+const contactsRouter = require("./routes/api/contacts"); 
 
-const app = express(); // create server
+const app = express(); 
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short"; // set format logger
+const formatsLogger = app.get("env") === "development" ? "dev" : "short"; 
 
-app.use(logger(formatsLogger)); // use logger
+app.use(logger(formatsLogger)); 
 app.use(cors());
 app.use(express.json()); 
 
-app.use("/api/contacts", contactsRouter); // use router
+app.use("/api/contacts", contactsRouter); 
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Error 404" }); // return 404 error if route not found
+  res.status(404).json({ message: "Error 404" });
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message }); // return server error
+  res.status(500).json({ message: err.message }); 
 });
 
 module.exports = app;
